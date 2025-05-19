e1baa960997e572426afb5c53338ad359fdf672f
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-05-19 09:01:43 -0400

    add plot

--- demo_trajectory.py
+++ demo_trajectory.py
@@ -8,6 +8,32 @@ if False:
   import logging
   logging.basicConfig(level=logging.INFO)
 
+def plot(t, y, figsize=(8.5, 12), facecolor='white', styles=None):
+
+  from matplotlib import pyplot as plt
+
+  n_stack = 1
+  if isinstance(y[0], list):
+    n_stack = len(y[0])
+  else:
+    y = [y]
+
+  plt.figure(figsize=figsize, facecolor=facecolor)
+  gs = plt.gcf().add_gridspec(n_stack)
+  axs = gs.subplots(sharex=True)
+
+  for i, _y in enumerate(y):
+    if isinstance(t[0], list):
+      _t = t[i]
+    if isinstance(styles, dict):
+      _styles = styles
+    else:
+      _styles = styles[i]
+
+    axs[i].plot(_t, _y, **_styles)
+    axs[i].grid(True)
+    #datetick()
+
 # TODO: Get from a query to SpacePy:
 #extMags = ['0', 'ALEX', 'MEAD', 'T87SHORT', 'T87LONG', 'T89', 'OPQUIET', 'OPDYN', 'T96', 'OSTA', 'T01QUIET', 'T01STORM', 'T05', 'TS07']
 extMags = ['0', 'MEAD', 'T87SHORT', 'T87LONG', 'T89', 'OPQUIET', 'T96', 'OSTA', 'T01STORM', 'T05', 'TS07']
@@ -15,6 +41,7 @@ extMags = ['0', 'MEAD', 'T87SHORT', 'T87LONG', 'T89', 'OPQUIET', 'T96', 'OSTA', 
 extMag = 'TS07'
 import pandas
 
+# 2015 June, Sept, Dec during storm periods total of 28 days
 dataframe = pandas.read_pickle("../satellite-predict/sat_data/data/cluster1_2001_avg_300_omni.pkl")
 for idx, row in dataframe.iterrows():
   x = row['x[km]']/6378.1
@@ -33,3 +60,4 @@ for idx, row in dataframe.iterrows():
     print(f"{extMag:11s} bx: {b[0]:.2f} by: {b[1]:.2f} bz: {b[2]:.2f}")
 
   break
+#plot([time], [[bx, by, bz]], styles={'label': 'measured', 'color': 'black', 'marker': '-', 'linestyle': 'None'})
